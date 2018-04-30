81fe07bf5929229f39a34ebb70f0c0fe1f0390d6
congyang <congyang@rayclear.com>
2018-04-30 14:37:03 +0800

    first commit 14:36

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
 from django.utils.six import python_2_unicode_compatible
 
-
 # python_2_unicode_compatible 装饰器用于兼容 Python2
+import markdown
+
+
 @python_2_unicode_compatible
 class Category(models.Model):
     """
@@ -84,6 +87,16 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_time']
